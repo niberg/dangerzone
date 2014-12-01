d13f7136134452ec9a5f355167df1cd3ddea8491
niberg <nicklas.bergman@gmail.com>
2014-12-01 22:53:12 +0100

    Badly coded interactive mode! Classify on the fly!

--- arffer/averagedperceptron.py
+++ arffer/averagedperceptron.py
@@ -6,6 +6,9 @@ import codecs
 import re
 import random
 import pickle
+from nltk.tokenize import word_tokenize, sent_tokenize
+from main import *
+import operator
 
 #Current weights
 weights = defaultdict(int)
@@ -17,67 +20,127 @@ timestamps = defaultdict(int)
 iterations = 4
 count = 0
 bias = 0
+interactive = False
+train = False
+test = False
+file = "dataset.arff"
+percentage = 100
 
 def main():
     global iterations
     global weights
     global bias
+    global interactive
+    global test
+    global train
+    global file
     
     try:
-        options, remainder = getopt.getopt(sys.argv[1:], 't:n:b:', ['train=', 'iterations=', 'bias='])
+        options, remainder = getopt.getopt(sys.argv[1:], 'd:n:b:itrf=', ['dataset=', 'iterations=', 'bias=', 'interactive', 'test=', 'train='])
     except getopt.GetoptError as err:
         # print help information and exit:
         print str(err) # will print something like "option -a not recognized"
         usage()
         
     for opt, arg in options:
-        if opt in ('-t', '--t'):
+        if opt in ('-d', '--dataset'):
             file = arg
         elif opt in ('-n', '--iterations'):
             iterations = int(arg)
         elif opt in ('-b', '--bias'):
             bias = float(arg)
+        elif opt in ('-i', '--interactive'):
+            interactive = True
+            if train or test:
+                print "Only one of parameters test, train and interactive can be given."
+                usage()             
+        elif opt in ('-t', '--test'):
+            test = True   
+            if train or interactive:
+                print "Only one of parameters test, train and interactive can be given."
+                usage()                
+        elif opt in ('-r', '--train'):
+            train = True    
+            if test or interactive:
+                print "Only one of parameters test, train and interactive can be given."
+                usage()     
+                
+
+    if interactive:
+        word_features, ngram_features = get_arff_features(file)
+        load()
+        print "Welcome to Perceptron 2000!"
+        while 1:
+            input = raw_input("Please input text to classify:\n\n")
+            input = unicode(input, "utf-8")
+            if not input:
+                sys.exit(0)
+            else:
+                features = extract_post_features(input, word_features, ngram_features)
+                prediction = predict(features)
+                if prediction:
+                    print "\nThe system classified this post as containing suicidal language.\n\n"
+                else:
+                    print "\nThe system classified this post as a rant.\n\n"
+                print "The perceptron recognized the following features: \n"
+                for word, value in word_features.iteritems():
+                    if value in features:
+                        sys.stdout.write(word + ", ")
+                print "\n\n"
+    if test:       
+        all_features = read_arff(file)
+        bin_features = binarize(all_features)
+        load()
+        true_positives = 0
+        false_positives = 0
+        true_negatives = 0
+        false_negatives = 0
+       
             
-    all_features = read_arff(file)
-    bin_features = binarize(all_features)
-    
-    true_positives = 0
-    false_positives = 0
-    true_negatives = 0
-    false_negatives = 0
-   
-    #Trains on ~66 % and tests on the rest
-    for i in range(iterations):
-        random.shuffle(bin_features)
-        for features, pclass in bin_features[:1318]:
-            learn(features, pclass)
-        average()
-        
-    for features, pclass in bin_features[1319:]:
-        prediction = predict(features)
+        for features, pclass in bin_features[1319:]:
+            prediction = predict(features)
 
-        if prediction == pclass:
-            if pclass == True:
-                true_positives += 1
+            if prediction == pclass:
+                if pclass == True:
+                    true_positives += 1
+                else:
+                    true_negatives += 1
             else:
-                true_negatives += 1
-        else:
-            if pclass == False:
-                false_positives += 1
-            else:
-                false_negatives += 1
+                if pclass == False:
+                    false_positives += 1
+                else:
+                    false_negatives += 1
 
-    print "True positives: " + str(true_positives)
-    print "True negatives: " + str(true_negatives)
-    print "False positives: " + str(false_positives)
-    print "False negatives: " + str(false_negatives)
+        print "True positives: " + str(true_positives)
+        print "True negatives: " + str(true_negatives)
+        print "False positives: " + str(false_positives)
+        print "False negatives: " + str(false_negatives)
         
-    print "Accuracy: " + str(float(true_positives)/(true_positives + false_positives)*100)
-    print "Recall: " + str(float(true_positives)/(false_negatives + true_positives)*100)
+        precision = float(true_positives)/(true_positives + false_positives)
+        recall = float(true_positives)/(false_negatives + true_positives)
+        fscore = 2 * ((precision * recall)/(precision + recall))
+        
+        print "Precision: " + str(round(precision*100, 2)) + " %"
+        print "Recall: " + str(round(recall*100, 2)) + " %"
+        print "F-score: " + str(round(fscore*100, 2)) + " %"
+        
+    if train:
+        all_features = read_arff(file)
+        bin_features = binarize(all_features)
+               
+
+        for i in range(iterations):
+            random.shuffle(bin_features)
+            for features, pclass in bin_features[:1318]:
+                learn(features, pclass)
+            average()
+            
+        save()
+        print "Successfully trained perceptron and saved weights to file."
     
     
 def usage():
-    print "Usage: --train <arff-file> [--iterations <n>] [--bias <n>]"
+    print "Usage: --dataset <arff-file> [--iterations <n>] [--bias <n>] [--interactive]"
     sys.exit(0)
     
 def predict(instance):
@@ -160,10 +223,79 @@ def load():
     global weights
     global cached_weights
     with codecs.open("model.sav", "rb") as file:
-        weights = pickle.load(file, -1)
-        cached_weights = pickle.load(file, -1)
+        weights = pickle.load(file)
+        cached_weights = pickle.load(file)
+        
+def extract_post_features(post, word_features, ngram_features):
+    #Assume post consists of one string
+    post_features = [0, 0, 0, 0, {}, {}]
+    sent_tokenized = sent_tokenize(post)
+    sent_word_tokenized = [word_tokenize(s) for s in sent_tokenized]
+    if ngram_features:
+        ngrams = len(ngram_features.keys()[0])
+    for sentence in sent_word_tokenized:
+        post_features[1] += 1
+        ngramsentence = find_ngrams(sentence, ngrams)
+        for ngram in ngramsentence:
+            if ngram in ngram_features.keys():
+                if ngram in post_features[5]:
+                    post_features[5][ngram] += 1
+                else:
+                    post_features[5][ngram] = 1
+        for token in sentence:
+            post_features[2] += 1
+            post_features[3] += len(token)
+            if token in word_features.keys():
+                if token in post_features[4]:
+                    post_features[4][token] += 1
+                else:
+                    post_features[4][token] = 1
+                    
+    post_features[3] = float(post_features[2]) / post_features[1]
+    post_features[2] = float(post_features[3]) / post_features[2]
+    
+    modified_list = [(0, 0), (1, post_features[1]), (2, post_features[2]), (3, post_features[3])]
+    for word, number in word_features.iteritems():
+        if word in post_features[4].keys():
+            modified_list.append((number, post_features[4][word]))
+    for ngram, number in ngram_features.iteritems():
+        if ngram in post_features[5].keys():
+            modified_list.append((number, post_features[5][ngram]))
 
     
+    return [x[0] for x in modified_list]
+    
+                
+        
+
+    
+
+def get_arff_features(file):
+    #word itself as key and attr number as value
+    word_features = {}
+    #ngram tuple as key and attr number as value
+    ngram_features = {}
+    with codecs.open(file, 'r', 'utf-8') as file:
+        lines = file.readlines()
+        for line in lines:
+            if "@ATTRIBUTE" in line:
+                if "word" in line and "%" in line:
+                    #Another ugly hack, everything after percentage is supposed to be the word
+                    word = line[line.find("%")+1:].strip()
+                    value = line.split()[1][4:].strip()
+                    word_features[word] = value
+                    continue
+                if "ngram" in line and "%" in line:
+                    ngram = tuple(line[line.find("%")+1:].strip().split())
+                    value = line.split()[1][5:]
+                    ngram_features[ngram] = value
+        # for word, key in word_features.iteritems():
+            # print word + " : " + key
+        # for ngram, key in ngram_features.iteritems():
+            # print ngram , " : " + key
+    return word_features, ngram_features
+       
+        
 if __name__ == "__main__":
     main()
         
