2a48abfd927f369eb172b441508749e2aa033545
niberg <nicklas.bergman@gmail.com>
2014-12-14 15:24:59 +0100

    Batch, bitches

--- BatchRedditDownloader.py
+++ BatchRedditDownloader.py
@@ -1,32 +1,73 @@
-import praw, codecs, sys
+import praw, codecs, sys, os, getopt
+from collections import defaultdict
+"""Use this to download posts en masse from specific :subreddits:. Set the limit of the total number of posts to go through with :limit:. Note that all posts will be downloaded to a folder "dataset" unless another folder is set with -f."""
 
-"""Use this to download posts en masse from a specific :subreddit:. Set the limit of number of posts to download with :limit:. Note that all posts will be downloaded to the current directory."""
-
-agent = "" # User agent for PRAW. Must be set.
-subreddit = "" # Must be a valid subreddit
-limit = 0 # Must be an integer > 0
+counts = defaultdict(int)
 
 def main():
-	validateVars()
-	r = praw.Reddit(user_agent=agent)
-	submissions = r.get_subreddit(subreddit).get_new(limit=limit)
-	writeSubmissionsToFile(submissions)
-	
-def writeSubmissionsToFile(submissions):
-	for post in submissions:
-		f = codecs.open(subreddit + '_' + str(post.id) + '.txt', 'w', 'utf-8')
-		f.write(post.title)
-		f.write("\n\n")
-		f.write(post.selftext)
-		f.close()
-	
-def validateVars():
-	if not subreddit:
-		print 'You must specify a subreddit to download posts from. Open this script in an editor to see info on how to do so.'
-		exit()
-	if not agent:
-		print 'You must specify a user agent name for PRAW. Open this script in an editor to see info on how to do so.'
-		exit()
-	if limit == 0:
-		print 'You must set the number of posts to download to a positive integer. Open this script in an editor to see info on how to do so.'
-		exit()
+    agent = "rmc" # User agent for PRAW. Must be set.
+    subs = ["rant", "suicidewatch", "personalfinance"] # Must be a valid list of subreddits
+    limit = 1000 # Must be an integer > 0
+    folder = "dataset"
+    getters = "get_new get_top get_top_from_year get_top_from_month get_top_from_week get_rising get_hot get_controversial".split()
+
+    try:
+        options, remainder = getopt.getopt(sys.argv[1:], 's:f:a:l:', ["subreddits=", "folder=", "useragent=", "limit="])
+    except getopt.GetoptError as err:
+        print str(err)
+        sys.exit(2)
+
+    for opt, arg in options:
+        if opt in ('-s', '--subreddits'):
+            subs = arg.split()
+        elif opt in ('-f', '--folder'):
+            folder = arg
+        elif opt in ('-a', '--useragent'):
+            agent = arg
+        elif opt in ('-l', '--limit'):
+            limit = int(arg)
+
+    r = praw.Reddit(user_agent=agent)
+    
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    for s in subs:
+        submissions = r.get_subreddit(s).get_new(limit=limit)
+        writeSubmissionsToFile(submissions, folder)
+        submissions = r.get_subreddit(s).get_top(limit=limit)
+        writeSubmissionsToFile(submissions, folder)   
+        submissions = r.get_subreddit(s).get_top_from_year(limit=limit)
+        writeSubmissionsToFile(submissions, folder)        
+        submissions = r.get_subreddit(s).get_top_from_month(limit=limit)
+        writeSubmissionsToFile(submissions, folder) 
+        submissions = r.get_subreddit(s).get_top_from_week(limit=limit)
+        writeSubmissionsToFile(submissions, folder)    
+        submissions = r.get_subreddit(s).get_rising(limit=limit)
+        writeSubmissionsToFile(submissions, folder)   
+        submissions = r.get_subreddit(s).get_hot(limit=limit)
+        writeSubmissionsToFile(submissions, folder)        
+        submissions = r.get_subreddit(s).get_controversial(limit=limit)
+        writeSubmissionsToFile(submissions, folder)                
+    print_counts(counts)
+    
+    
+def writeSubmissionsToFile(submissions, folder):
+        global counts
+        for post in submissions:
+            filename = post.subreddit.display_name.lower() + '_' + str(post.id) + '.txt'
+            joinedpath = os.path.join(os.getcwd() + "/" + folder + "/", filename)
+            if not os.path.isfile(joinedpath):
+                counts[post.subreddit.display_name.lower()] += 1
+                f = codecs.open(joinedpath, 'w', 'utf-8')
+                f.write(post.title)
+                f.write("\n\n")
+                f.write(post.selftext)
+                f.close()
+
+def print_counts(counts):
+    for key, freq in counts.iteritems():
+        print "Found", freq, "posts from", key + "."
+        
+        
+if __name__ == "__main__":    
+    main()
